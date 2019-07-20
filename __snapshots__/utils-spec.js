25daad8a70985db9b5bd7da310fe9459fb362620
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-07-20 15:09:38 -0400

    test detecting utc timestamp strings

--- __snapshots__/utils-spec.js
+++ __snapshots__/utils-spec.js
@@ -13,3 +13,9 @@ exports['utils timestampRegex detects timestamp 1'] = {
   "2019-07-19T02:03:45.827Z compute 5 + 10 = 15": true,
   "2019-02-01 some other format": false
 }
+
+exports['utils utcTimestampRegex detects utc timestamp strings 1'] = {
+  "Sat, 20 Jul 2019 19:08:47 GMT compute 2 + 3 = 5": true,
+  "2019-07-19T02:03:45.827Z compute 5 + 10 = 15": false,
+  "2019-02-01 some other format": false
+}
--- test/utils-spec.js
+++ test/utils-spec.js
@@ -18,6 +18,18 @@ describe('utils', () => {
     })
   })
 
+  context('utcTimestampRegex', () => {
+    it('detects utc timestamp strings', () => {
+      const strings = [
+        'Sat, 20 Jul 2019 19:08:47 GMT compute 2 + 3 = 5',
+        '2019-07-19T02:03:45.827Z compute 5 + 10 = 15',
+        '2019-02-01 some other format',
+      ]
+      const detected = strings.map(s => utils.utcTimestampRegex.test(s))
+      snapshot(R.zipObj(strings, detected))
+    })
+  })
+
   context('removeNamespaceAndPid', () => {
     it('removes namespace and pid', () => {
       const text = 'VERBOSE 177: this is verbose debug = 42'
